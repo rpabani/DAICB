4fc740faa992b13436df683c327cd25f29069c67
Rizwan Pabani <38048562-rizpabani@users.noreply.replit.com>
2024-11-27 10:13:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+  const { createClient } = require('@supabase/supabase-js')
+
+const supabase = createClient(
+  'https://mftkvfwrahznqhgenmcg.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mdGt2ZndyYWh6bnFoZ2VubWNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2MjYwMTksImV4cCI6MjA0ODIwMjAxOX0.QzGaxAT272A2kQOlUxKFsTZTZWcYfraN89RfW5XReLo'
+)
+
+
